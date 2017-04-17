10a93280254a94a726b5098af465a2e35a0233b0
ambey <satyamyuvasoft158@gmail.com>
2017-04-17 18:44:09 +0530

    feateure setting up action cabel

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+config.web_socket_server_url = "wss://test15-app.herokuapp.com/cable" 
+
+config.action_cable.allowed_request_origins = ['https://test15-app.herokuapp.com', 'http://test15-app.herokuapp.com']
 end
